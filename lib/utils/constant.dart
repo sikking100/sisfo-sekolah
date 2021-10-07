d0d58636439b357bc96e112e5014f74f68fd495b
Sigit Hadi Prawira <sigithadiprawira@gmail.com>
2021-10-07 10:27:59 +0800

    update id of admin

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -4,4 +4,4 @@ import 'package:flutter/material.dart';
 const List<String> listKelas = ['', '1', '2', '3', '4', '5', '6'];
 const Icon delete = Icon(Icons.delete, color: Colors.red);
 const Icon edit = Icon(Icons.edit, color: Colors.blue);
-const String id = 'DdIORBTR4GdQ3Eedmkso9Lkxvxl2';
+const String id = '0kPEL60XUmbhQyfFglLwDXp6xVi1';
